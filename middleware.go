de4a10ee9bf4fc721997aa47da0eb96bcb34797e
Jeffrey Walter <jeffreydwalter@gmail.com>
2021-09-13 01:55:29 -0500

    Update middleware.go

--- middleware.go
+++ middleware.go
@@ -3,7 +3,6 @@ package oauth
 import (
 	"context"
 	"errors"
-	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -12,11 +11,11 @@ import (
 type contextKey string
 
 const (
-	credential  contextKey = "oauth.credential"
-	claims      contextKey = "oauth.claims"
-	scope       contextKey = "oauth.scope"
-	tokenType   contextKey = "oauth.tokentype"
-	accessToken contextKey = "oauth.accesstoken"
+	CredentialContext  contextKey = "oauth.credential"
+	ClaimsContext      contextKey = "oauth.claims"
+	ScopeContext       contextKey = "oauth.scope"
+	TokenTypeContext   contextKey = "oauth.tokentype"
+	AccessTokenContext contextKey = "oauth.accesstoken"
 )
 
 // BearerAuthentication middleware for go-chi
@@ -45,9 +44,7 @@ func Authorize(secretKey string, formatter TokenSecureFormatter) func(next http.
 // Token is retrieved from the Authorization HTTP header that respects the format
 // Authorization: Bearer {access_token}
 func (ba *BearerAuthentication) Authorize(next http.Handler) http.Handler {
-	log.Print("Authorize middleware")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Print("Authorize handler")
 		auth := r.Header.Get("Authorization")
 		token, err := ba.checkAuthorizationHeader(auth)
 		if err != nil {
@@ -56,11 +53,11 @@ func (ba *BearerAuthentication) Authorize(next http.Handler) http.Handler {
 		}
 
 		ctx := r.Context()
-		ctx = context.WithValue(ctx, credential, token.Credential)
-		ctx = context.WithValue(ctx, claims, token.Claims)
-		ctx = context.WithValue(ctx, scope, token.Scope)
-		ctx = context.WithValue(ctx, tokenType, token.TokenType)
-		ctx = context.WithValue(ctx, accessToken, auth[7:])
+		ctx = context.WithValue(ctx, CredentialContext, token.Credential)
+		ctx = context.WithValue(ctx, ClaimsContext, token.Claims)
+		ctx = context.WithValue(ctx, ScopeContext, token.Scope)
+		ctx = context.WithValue(ctx, TokenTypeContext, token.TokenType)
+		ctx = context.WithValue(ctx, AccessTokenContext, auth[7:])
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
